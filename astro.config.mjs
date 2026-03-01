e00fdffb1da14071f294ab7e769eeae9b9a6181f
Wilkesboro Team <admin@wilkesborotoday.com>
2026-03-01 10:12:51 +0800

    Switch to Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
-  site: 'https://wilkesboro.today',
-  build: {
-    format: 'directory'
-  }
+  adapter: vercel(),
+  site: 'https://wilkesboro.vercel.app',
 });
